66d36db124f1a21bb3b9afdcb1bf740e46bb23c1
taka4949 <gogigaga1101@outlook.jp>
2026-02-10 16:58:22 +0900

    441cラングレス圧縮

--- src/Main.kt
+++ src/Main.kt
@@ -5,39 +5,35 @@ import kotlin.math.*
 import kotlin.math.min
 import java.util.ArrayList
 import java.lang.Math.*
+import kotlin.math.max
 
 fun main() {
-    val input1 = readLine()!!.split(" ")
-    val n = input1[0].toInt()
-    val m = input1[1].toInt()
-
-    val s = Array(m){ IntArray(0) }
-    for(i in 0 until m){
-        val l = readLine()!!.split(" ").map { it.toInt() }
-        s[i] = l.subList(1,l.size).map{it - 1}.toIntArray()
+    val s = readLine()!!
+    val n = s.length
+    if (n == 0) {
+        println(0)
+        return
     }
 
-    val p = readLine()!!.split(" ").map { it.toInt() }
-    var ans = 0
-
-    for(bit in 0 until (1 shl n)){
-        var a = true
-        for(i in 0 until m){
-            var counton = 0 // ここで電球を１つ１つカウントリセット
-            for(sw in s[i]){
-                if(((bit shr sw) and 1 ) == 1){  // andでも（）必須！。 スイッチではなく、ビットシートをスイッチまで動かすイメージ
-                    counton ++ //ここでonをカウント
-                }
-            }
-            if(counton % 2 != p[i]){
-                a = false
-                break
-            }
+    val v = mutableListOf<Int>()
+    val l = mutableListOf<Int>()
+    var i = 0
+    while (i < n) {
+        var j = i
+        while (j < n && s[j] == s[i]) {
+            j++
         }
-        if(a)
-            ans++
+        v.add(s[i] - '0')
+        l.add(j - i)
+        i = j
     }
-    println(ans)
+
+    var a: Long = 0
+    for (k in 0 until v.size - 1) {
+        if (v[k] + 1 == v[k + 1]) {
+            val m = if (l[k] < l[k + 1]) l[k] else l[k + 1]
+            a += m.toLong()
+        }
+    }
+    println(a)
 }
-//　うえから、００や０１のシートを電球につくスイッチなどがミルフィーユ状ある、それを重ねる。その横に＝あまりが偶数か奇数かを判定する物が付属している。
-//　ビット全探索はＮが20以下！、2択
