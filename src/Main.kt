d253a162f14cdd9ba27d3b54879ec81358d9da5f
taka4949 <gogigaga1101@outlook.jp>
2026-02-18 23:16:31 +0900

    209c　大事　

--- src/Main.kt
+++ src/Main.kt
@@ -1,36 +1,34 @@
 import kotlin.math.abs
 import kotlin.math.*
 import kotlin.math.min
+import java.util.Scanner
 import java.util.ArrayList
 import java.lang.Math.*
 import kotlin.math.max
 import java.util.Arrays
 
 fun main () {
-    val l1 = readLine()!!.split(" ")
+    val n = readLine()!!.toInt()
 
-    val n = l1[0].toInt()
-    val q = l1[1].toInt()
+    val c = readLine()!!.split(" ").map { it.toLong() }.sorted()
 
-    val s = HashSet<Pair<Int,Int>>() // 中身は（１，２）や（２，１）など
+    var ans = 1L
+    val mod = 1000000007L
 
-    for (i in 0 until q) {
-        val line = readLine()!!.split(" ")
-        val t = line[0].toInt()
-        val a = line[1].toInt()
-        val b = line[2].toInt()
+    for (i in 0 until n) {
+        // c[i] : その人の上限値（本来選べる候補の数）
+        // i    : その人より前にいる人数（すでに使われてしまった数字の個数）3!（3×2×1)のようなもの
+        val count = c[i] - i
 
-        if(t == 1){
-            s.add(a to b)
-        }else if(t == 2){
-            s.remove(a to b)
-        }else{
-            if(s.contains(a to b) && s.contains(b to a)){
-                println("Yes")
-            }else{
-                println("No")
-            }
+        // もし (上限 - 前の人数) が 0以下なら、選べる数字がないということ
+        if (count <= 0) {
+            println(0)
+            return
         }
+
+        ans = (ans * count) % mod
     }
+
+    println(ans)
 }
 
