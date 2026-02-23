88ee361066c05616634e5e4c474f8d2f1a7079c7
taka4949 <gogigaga1101@outlook.jp>
2026-02-23 21:35:16 +0900

    264b

--- src/Main.kt
+++ src/Main.kt
@@ -7,45 +7,25 @@ import java.util.ArrayList
 import java.lang.Math.*
 import kotlin.math.max
 import java.util.Arrays
+fun main() {
+        val s = readLine()!!.split(" ")
+        val r = s[0].toInt()
+        val c = s[1].toInt()
 
-fun main () {
-    val t = readLine()!!.toInt()
-    repeat(t) {
-        val (n, d) = readLine()!!.split(" ").map { it.toInt() }
-        val a = readLine()!!.split(" ").map { it.toLong() }
-        val b = readLine()!!.split(" ").map { it.toLong() }
+        // 中心(8, 8)からの距離を計算
+        // abs()だけで書けるから見た目がスッキリする
+        val h = abs(r - 8)
+        val v = abs(c - 8)
 
-        val q = mutableListOf<LongArray>()
-        var u = 0
-        var e = 0
+        // 大きい方の距離（何層目か）を出す
+        val x = if (h > v) h else v
 
-        for (i in 1..n) {
-
-            q.add(longArrayOf(i.toLong(), a[i - 1]))
-            var r = b[i - 1]
-            while (r > 0 && u < q.size) {
-                val f = q[u]
-                if (f[1] <= r) {
-                    r -= f[1]
-                    f[1] = 0
-                    u++
-                } else {
-                    f[1] -= r
-                    r = 0
-                }
-            }
-            val l = i - d
-            while (e < q.size && q[e][0] <= l) {
-                q[e][1] = 0
-                e++
-            }
-            if (u < e) u = e
+        // 奇数なら黒、偶数なら白
+        if (x % 2 != 0) {
+            println("black")
+        } else {
+            println("white")
         }
-
-        var s = 0L
-        for (j in 0 until q.size) s += q[j][1]
-        println(s)
     }
 
-}
 
