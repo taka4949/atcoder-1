0f5d0f05ccbde9b5a2632a81d2682d4d3d4d9dcd
taka4949 <gogigaga1101@outlook.jp>
2026-02-27 11:08:51 +0900

    204

--- src/Main.kt
+++ src/Main.kt
@@ -8,24 +8,36 @@ import java.lang.Math.*
 import kotlin.math.max
 import java.util.Arrays
 fun main() {
-        val s = readLine()!!.split(" ")
-        val r = s[0].toInt()
-        val c = s[1].toInt()
+        val l = readLine()!!.split(" ")
+        val n = l[0].toInt()
+        val m = l[1].toInt()
+        val g = Array(n + 1) { mutableListOf<Int>() }
 
-        // 中心(8, 8)からの距離を計算
-        // abs()だけで書けるから見た目がスッキリする
-        val h = abs(r - 8)
-        val v = abs(c - 8)
-
-        // 大きい方の距離（何層目か）を出す
-        val x = if (h > v) h else v
-
-        // 奇数なら黒、偶数なら白
-        if (x % 2 != 0) {
-            println("black")
-        } else {
-            println("white")
+        for (i in 0 until m) {
+            val e = readLine()!!.split(" ")
+            val u = e[0].toInt()
+            val v = e[1].toInt()
+            g[u].add(v)
         }
+
+        var t = 0
+        for (i in 1..n) {
+            val v = BooleanArray(n + 1)
+            t += f1(i, g, v)
+        }
+        println(t)
     }
+
+    fun f1(c: Int, g: Array<MutableList<Int>>, v: BooleanArray): Int {
+        v[c] = true
+        var r = 1
+        for (n in g[c]) {
+            if (!v[n]) {
+                r += f1(n, g, v)
+            }
+        }
+        return r
+    }
+
 
 
