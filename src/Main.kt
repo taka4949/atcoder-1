81e8ab07b94212152510e40053badfb055413211
taka4949 <gogigaga1101@outlook.jp>
2026-02-28 23:21:43 +0900

    446c

--- src/Main.kt
+++ src/Main.kt
@@ -7,37 +7,25 @@ import java.util.ArrayList
 import java.lang.Math.*
 import kotlin.math.max
 import java.util.Arrays
-fun main() {
-        val l = readLine()!!.split(" ")
-        val n = l[0].toInt()
-        val m = l[1].toInt()
-        val g = Array(n + 1) { mutableListOf<Int>() }
 
-        for (i in 0 until m) {
-            val e = readLine()!!.split(" ")
-            val u = e[0].toInt()
-            val v = e[1].toInt()
-            g[u].add(v)
-        }
 
-        var t = 0
-        for (i in 1..n) {
-            val v = BooleanArray(n + 1)
-            t += f1(i, g, v)
+fun main () {
+    val n = readLine()!!.toInt()
+    val m = readLine()!!.split(" ").map { it.toInt() }
+    val l = IntArray(n + 1)
+
+    for (i in n downTo 1) {
+        val x = m[i - 1]
+        if (x == i) {
+            l[i] = i
+        } else {
+            l[i] = l[x]
         }
-        println(t)
     }
 
-    fun f1(c: Int, g: Array<MutableList<Int>>, v: BooleanArray): Int {
-        v[c] = true
-        var r = 1
-        for (n in g[c]) {
-            if (!v[n]) {
-                r += f1(n, g, v)
-            }
-        }
-        return r
-    }
+    println((1..n).map { l[it] }.joinToString(" "))
+}
+
 
 
 
