ae664344a1524d82b84c814f9a93c183c51080ae
taka4949 <gogigaga1101@outlook.jp>
2026-03-01 21:55:07 +0900

    メモ化フィボナッチ

--- src/Main.kt
+++ src/Main.kt
@@ -8,22 +8,25 @@ import java.lang.Math.*
 import kotlin.math.max
 import java.util.Arrays
 
+// メモ用の配列を用意（0以外の数字が入っていたら「計算済み」とみなす）
+val m = LongArray(100)
 
+fun f(n: Int): Long {
+    // 0と1はそのまま返す
+    if (n == 0) return 0
+    if (n == 1) return 1
+
+    // もしメモに結果があるなら、計算しないでそれを返す！
+    if (m[n] != 0L) return m[n]
+
+    // なければ計算して、メモに保存する
+    m[n] = f(n - 1) + f(n - 2)
+    return m[n]
+}
 fun main () {
     val n = readLine()!!.toInt()
-    val m = readLine()!!.split(" ").map { it.toInt() }
-    val l = IntArray(n + 1)
+    println(f(n))
 
-    for (i in n downTo 1) {
-        val x = m[i - 1]
-        if (x == i) {
-            l[i] = i
-        } else {
-            l[i] = l[x]
-        }
-    }
-
-    println((1..n).map { l[it] }.joinToString(" "))
 }
 
 
