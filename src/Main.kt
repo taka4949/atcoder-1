629232e8ed10a17c390236ce0ea589ea119f2221
taka4949 <gogigaga1101@outlook.jp>
2026-03-02 18:53:48 +0900

    284D素因数分解

--- src/Main.kt
+++ src/Main.kt
@@ -4,29 +4,31 @@ import kotlin.math.min
 import java.util.*
 import java.util.Scanner
 import java.util.ArrayList
-import java.lang.Math.*
 import kotlin.math.max
 import java.util.Arrays
 
-// メモ用の配列を用意（0以外の数字が入っていたら「計算済み」とみなす）
-val m = LongArray(100)
-
-fun f(n: Int): Long {
-    // 0と1はそのまま返す
-    if (n == 0) return 0
-    if (n == 1) return 1
-
-    // もしメモに結果があるなら、計算しないでそれを返す！
-    if (m[n] != 0L) return m[n]
-
-    // なければ計算して、メモに保存する
-    m[n] = f(n - 1) + f(n - 2)
-    return m[n]
-}
 fun main () {
-    val n = readLine()!!.toInt()
-    println(f(n))
+   val t = readLine()!!.toInt() //この問題文の制約→必ずqかp見つかったら、片方も存在する
+    repeat(t){
+        val n = readLine()!!.toLong() //45の場合
+        var p = 0L
+        var q = 0L
 
+        for(i in 2L..3000000L){ //三十根の値以上は調べる必要なし
+            if(n % i == 0L){ //3
+                if((n / i) % i == 0L){ //5
+                    p = i
+                    q = n / (p * p)
+                }else{
+                    q = i
+                    val res = (n / q).toDouble()
+                    p = sqrt(res).roundToLong() //sqrtはdouble型のみ受け付ける
+                }
+                break
+            }
+        }
+        println("$p $q")
+    }
 }
 
 
