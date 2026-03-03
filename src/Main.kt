02e31dc9119545a60dc9c8bf0209171b3b52033d
taka4949 <gogigaga1101@outlook.jp>
2026-03-03 10:51:47 +0900

    約数列挙

--- src/Main.kt
+++ src/Main.kt
@@ -8,27 +8,21 @@ import kotlin.math.max
 import java.util.Arrays
 
 fun main () {
-   val t = readLine()!!.toInt() //この問題文の制約→必ずqかp見つかったら、片方も存在する
-    repeat(t){
-        val n = readLine()!!.toLong() //45の場合
-        var p = 0L
-        var q = 0L
+    val n = readLine()!!.toLong()
+    val r = mutableListOf<Long>()
 
-        for(i in 2L..3000000L){ //三十根の値以上は調べる必要なし
-            if(n % i == 0L){ //3
-                if((n / i) % i == 0L){ //5
-                    p = i
-                    q = n / (p * p)
-                }else{
-                    q = i
-                    val res = (n / q).toDouble()
-                    p = sqrt(res).roundToLong() //sqrtはdouble型のみ受け付ける
-                }
-                break
-            }
-        }
-        println("$p $q")
+    var i = 1L
+    while(i * i <= n){ //√nまで調べれば問題ない。約数列挙
+       if(n % i == 0L){
+           r.add(i)
+           if(i * i != n){ //i = 1の場合、６もいれる。ｎ＝16の場合、４を2つ入れないように防いでいる
+               r.add(n / i)
+           }
+       }
+        i++
     }
+    r.sort()
+    println(r.joinToString("\n"))
 }
 
 
