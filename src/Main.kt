bbbccb3c455aec209425f9a8febd1cbfac479a02
taka4949 <gogigaga1101@outlook.jp>
2026-03-03 16:06:25 +0900

    215c深さ優先探索

--- src/Main.kt
+++ src/Main.kt
@@ -7,21 +7,34 @@ import java.util.ArrayList
 import kotlin.math.max
 import java.util.Arrays
 
-fun main () {
- val n = readLine()!!.toInt()
-    val a = readLine()!!.split(" ").map { it.toLong() }.toLongArray()
+fun main() {
+    val inStr = readLine()!!.split(" ")
+    val s = inStr[0]
+    val k = inStr[1].toInt()
 
-    val c = LongArray(200)
-    for(x in a){
-        c[(x % 200).toInt()]++ //あまりのペアを作る。差が倍数＝あまりが同じ。401と201なら余り１、ペア成立。
+    val set = mutableSetOf<String>()
+    val u = BooleanArray(s.length)
+
+    fun dfs(c: String) {
+        if (c.length == s.length) {
+            set.add(c)
+            return
+        }
+        for (i in 0 until s.length) {
+            if (!u[i]) {
+                u[i] = true
+                dfs(c + s[i]) //スタックポインタの巻き戻し。リターン後は、ここはすでに存在しないため、2文字からスタート。
+                u[i] = false //戻り先アドレス。リターン後に実行。ループが最後までいったら、ひとつ前に戻る。
+            }
+        }
     }
-    var ans = 0L
-    for(i in c){
-        ans += i * (i - 1) / 2 //2以上＝1ペア以上
-    }
-    println(ans)
+
+    dfs("")
+    val l = set.toList().sorted()
+    println(l[k - 1])
 }
 
 
+// 深さ優先探索。例aab→012→021→102→120→201→210。3階層というなら、後ろの階層が最後までおわったら手前に戻り、最後まで進む。
 
 
