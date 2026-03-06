bf0e2a2dcd66500fbb33260290e9788d68a6fa6d
taka4949 <gogigaga1101@outlook.jp>
2026-03-06 20:09:37 +0900

    253c,treemapはソートと計算量激減map

--- src/Main.kt
+++ src/Main.kt
@@ -8,41 +8,31 @@ import kotlin.math.max
 import java.util.Arrays
 
 fun main() {
-    val nq = readLine()!!.split(" ")
-    val n = nq[0].toInt()
-    val q = nq[1].toInt()
+  val q = readLine()!!.toInt()
+    val m = TreeMap<Int,Int>() //最小、最大を容易に調べられる。全データ見る必要がない、ノッド型。
 
-    val a = readLine()!!.split(" ")
-
-    val m = mutableMapOf<Int, MutableList<Int>>()//マップは空、作る必要がある。
-
-    for(i in 0 until n){
-        val num = a[i].toInt()
-        val p = i + 1//0番目は1番目にしたい。もし、0番目にした場合、リストには0に0番目が入る。出力時に１ではなく０を出してしまう。
-
-        if(m.containsKey(num)){
-            m[num]!!.add(p)
-        }else{
-            val l = mutableListOf<Int>() //マップにリストを入れる場合、ここで作る。
-            l.add(p)//リアロケーションしている。
-            m[num] = l//マップにリストを入れる方法
-        }
-    }
     for(i in 0 until q){
-        val q1 = readLine()!!.split(" ")
-        val x = q1[0].toInt()
-        val y = q1[1].toInt()
+        val l = readLine()!!.split(" ")
+        val t = l[0].toInt()
 
-        if(m.containsKey(x)){
-            val x1 = m[x]!!
-            if(y <= x1.size){
-                println(x1[y - 1])//1番目はリストでは0番目なため。
-            }else{
-                println("-1")
+        if(t == 1){
+            val x = l[1].toInt()
+            m[x] = m.getOrDefault(x,0) + 1
+        }else if(t == 2) {
+            val x = l[1].toInt()
+            val y = l[2].toInt()
+            if(m.containsKey(x)){
+                val cut = m[x]!!
+                if(cut <= y){//かりに２＜＝１なら、２－１、２＜＝３なら、全削除。
+                    m.remove(x)
+                } else{
+                    m[x] = cut - y
+                }
             }
-        }else{
-            println("-1")
+        }else if(t == 3){
+            val ma = m.lastKey()//treemapの特徴
+            val mi = m.firstKey()
+            println(ma - mi)
         }
     }
-
-}
+    }
