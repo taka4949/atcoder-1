4164343769e1ca72aa664a77e60f34b17f630504
taka4949 <gogigaga1101@outlook.jp>
2026-03-07 19:01:33 +0900

    224c、傾きとは。割り算は誤差でる

--- src/Main.kt
+++ src/Main.kt
@@ -9,19 +9,32 @@ import java.util.Arrays
 import java.util.ArrayDeque
 
 fun main() {
- val n = readLine()!!.toInt()
-    val s = readLine()!!
+  val n = readLine()!!.toInt()
+    val x = LongArray(n)
+    val y = LongArray(n)
 
-    val q = ArrayDeque<Int>()//両端に値を入れられる、筒。問題は追加していく際のみ値の隣に置く操作。
-    q.addFirst(n)//逆順。前から入れていくと、割り込みする必要があり、隙間を開ける作業がある。計算量が増加する。
+    for(i in 0 until n){
+        val s = readLine()!!.split(" ")
+        x[i] = s[0].toLong()
+        y[i] = s[1].toLong()
+    }
 
-    for(i in n - 1 downTo 0){
-        if(s[i] == 'L'){//逆順のため、１２３０なら、２は１のRだが、３のL。3スタートなため。
-            q.addLast(i)//親から子グループではなく、子グループから親を作っていく。
-        }else{
-            q.addFirst(i)
+    var ans = 0L
+    for(i in 0 until n - 2){
+        for(j in i + 1 until n - 1){
+            for(k in j + 1 until n){
+                val x1 = x[i] - x[j]
+                val y1 = y[i] - y[j]
+                val x2 = x[i] - x[k]
+                val y2 = y[i] - y[k]
+//傾きとは、変化の割合。ｘとｙの2地点の距離からｘ÷ｙしたもの。距離ではなく割合！。
+                if(x1 * y2 != x2 * y1){//仮に同じ値なら、分母を消しても分子を消しても同じ値になる。
+                    ans++ //↑両方の値に、分母を消すために、両方の分母をかける。
+                }
+
+            }
         }
     }
-    println(q.joinToString(" "))
-    }
-//数字がでかいものほど、内側に来る。外から内では割り込みが必須。内から外なら両端に入れるだけで済む！！。
+    println(ans)
+}
+//ＰＧは分数不可、割り算だと誤差が生じるため厳密に＝＝したい場合は, 避けるべき。
