d925a33bfc146540ea0ebe10c5e6d5a34cfe5b19
taka4949 <gogigaga1101@outlook.jp>
2026-03-11 12:52:45 +0900

    227C約数範囲は、最大値の範囲

--- src/Main.kt
+++ src/Main.kt
@@ -10,23 +10,22 @@ import java.util.ArrayDeque
 import kotlin.math.max
 
 fun main() {
-    val n = readLine()!!.toInt()
-    val p = readLine()!!.split(" ").map { it.toInt() }
+val n = readLine()!!.toLong()//4
+    var ans = 0L
 
-    val c = IntArray(n)
-
-    for(i in 0 until n){
-        val x = ((p[i] - 1) - i + n) % n //p[i]=人iとi(料理の現在地）から引いた距離＝円卓の回転数
-        val y = (p[i] - i + n ) % n
-        val z = ((p[i] + 1) - i + n) % n //+nで３から０へ。-3を返さないようにする！。
-
-        c[x]++
-        c[y]++
-        c[z]++
+    var a = 1L
+    while(a * a * a <= n){//2ならそもそも無理
+        var b = a//a<=b
+        while(a * b * b <= n){//3なら無理。B<=Cなため。
+            val m = n / (a * b)//a*bと何をかければｎになるのか？（ｘ＝ｃ）
+            ans += (m - b + 1)//ｃとｂの範囲が個数、＜＝なため、ｂも含めるために＋１
+            b++
+        }
+        a++
     }
-    var ans = 0
-    for(i in 0 until n){
-        ans = max(ans,c[i])//回転数が一番多い→料理全パターンで最も希望者が多い回転してほしい数。
-    }
-    println(ans)
+ println(ans)
 }
+//ループ内で判定は、cpuのパイプライン処理が乱れる。
+//cpuは、割り算ではなく引き算をしている。そのため、割り算は重い。
+//a,b=1のときは、a*b*x=n→x=n/a*bでx=c=4。ｂとｃの範囲は４だから4通り。a1,b2のとき、4/2=2=c。2と2の範囲は１。
+//何通り？→最も範囲が狭いであろう最大値（ｃ）の範囲から考える
