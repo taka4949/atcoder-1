fa7c460ef9bca56f354f0a006a5c183eb03b5541
taka4949 <gogigaga1101@outlook.jp>
2026-03-11 13:51:16 +0900

    289cビット全探索とset、配列

--- src/Main.kt
+++ src/Main.kt
@@ -10,22 +10,28 @@ import java.util.ArrayDeque
 import kotlin.math.max
 
 fun main() {
-val n = readLine()!!.toLong()//4
-    var ans = 0L
-
-    var a = 1L
-    while(a * a * a <= n){//2ならそもそも無理
-        var b = a//a<=b
-        while(a * b * b <= n){//3なら無理。B<=Cなため。
-            val m = n / (a * b)//a*bと何をかければｎになるのか？（ｘ＝ｃ）
-            ans += (m - b + 1)//ｃとｂの範囲が個数、＜＝なため、ｂも含めるために＋１
-            b++
-        }
-        a++
+    val (n, m) = readLine()!!.split(" ").map { it.toInt() }
+    val a = Array(m) { IntArray(0) }
+    for (i in 0 until m) {
+        readLine()
+        a[i] = readLine()!!.split(" ").map { it.toInt() }.toIntArray()
     }
- println(ans)
+
+    var ans = 0
+    for (i in 1 until (1 shl m)) {
+        val st = mutableSetOf<Int>()//重要
+
+        for (j in 0 until m) {
+            if (((i shr j) and 1) == 1) {
+                for (x in a[j]) {
+                    st.add(x)//a[j]の値を一つ一つ入れていく
+                }
+            }
+        }
+        if (st.size == n) {
+            ans++
+        }
+    }
+    println(ans)
 }
-//ループ内で判定は、cpuのパイプライン処理が乱れる。
-//cpuは、割り算ではなく引き算をしている。そのため、割り算は重い。
-//a,b=1のときは、a*b*x=n→x=n/a*bでx=c=4。ｂとｃの範囲は４だから4通り。a1,b2のとき、4/2=2=c。2と2の範囲は１。
-//何通り？→最も範囲が狭いであろう最大値（ｃ）の範囲から考える
+
