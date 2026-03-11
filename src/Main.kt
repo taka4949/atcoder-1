d6d28131de8ceeb9044823fd2bc6616e46622f99
taka4949 <gogigaga1101@outlook.jp>
2026-03-11 18:18:06 +0900

    232c深さ優先、再帰の考え方

--- src/Main.kt
+++ src/Main.kt
@@ -10,28 +10,57 @@ import java.util.ArrayDeque
 import kotlin.math.max
 
 fun main() {
-    val (n, m) = readLine()!!.split(" ").map { it.toInt() }
-    val a = Array(m) { IntArray(0) }
+    val s = readLine()!!.split(" ")
+    val n = s[0].toInt()
+    val m = s[1].toInt()
+
+  val a = Array(n){ BooleanArray(n) }
+    val b = Array(n){ BooleanArray(n) }
+
+    for(i in 0 until m){
+        val t = readLine()!!.split(" ")
+        val u = t[0].toInt() -1
+        val v = t[1].toInt() -1
+        a[u][v] = true
+        a[v][u] = true
+    }
     for (i in 0 until m) {
-        readLine()
-        a[i] = readLine()!!.split(" ").map { it.toInt() }.toIntArray()
+        val t = readLine()!!.split(" ")
+        val u = t[0].toInt() - 1
+        val v = t[1].toInt() - 1
+        b[u][v] = true
+        b[v][u] = true
     }
 
-    var ans = 0
-    for (i in 1 until (1 shl m)) {
-        val st = mutableSetOf<Int>()//重要
+     var ans = false
+    val p = IntArray(n)
+    val used = BooleanArray(n)
 
-        for (j in 0 until m) {
-            if (((i shr j) and 1) == 1) {
-                for (x in a[j]) {
-                    st.add(x)//a[j]の値を一つ一つ入れていく
+    fun dfs(d:Int){
+        if(d == n) {
+            var ok = true
+            for (i in 0 until n) {
+                for (j in 0 until n) {
+                    if (a[i][j] != b[p[i]][p[j]]) {
+                        ok = false
+                    }
                 }
             }
+            if (ok)  ans = true
+            return
         }
-        if (st.size == n) {
-            ans++
+        for(i in 0 until n){
+            if(!used[i]){//大事
+                used[i] = true
+                p[d] = i
+                dfs(d + 1)
+                used [i] = false
+            }
         }
     }
-    println(ans)
+    dfs(0)
+    println(if(ans) "Yes" else "No")
 }
-
+//dfs１から３まで行く場合は最後までループするが、３から2へ戻る際などは最初に進んでいたループの途中からスタートする。
+//a基準に考える。インデックスの位置が対応関係。紐の本数は変化だめ。
+//nの大きさが小さい場合、対応関係を探す場合、深さ優先探索
