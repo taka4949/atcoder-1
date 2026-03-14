c99e1d9b662641b5251e50c271abf26507ecb1db
taka4949 <gogigaga1101@outlook.jp>
2026-03-14 22:54:41 +0900

    449c文字集計を二分で

--- src/Main.kt
+++ src/Main.kt
@@ -11,13 +11,27 @@ import kotlin.math.max
 import kotlin.system.exitProcess
 
 fun main() {
-    val  (l,r) = readLine()!!.split(" ").map { it.toLong() }
-    val g = gcd(l,r)//12，24なら12が最大公約数＋重複していないのは２→24答え
-    val ans = (l * r ) / g//最小公倍数とは、両方の共通する数字を１つずつかけ合わせた数字。
-    println(ans)
+    val (n,l,r) = readLine()!!.split(" ").map { it.toInt() }
+    val s = readLine()!!
 
-}
+    val a = Array(26) { mutableListOf<Int>() }
+    for (i in 0 until n) {
+        a[s[i] - 'a'].add(i + 1)//s内a等のインデックス番号をリストへ
+    }
+    var count = 0L
+    for (i in 0 until 26) {
+        val v = a[i]
 
-fun gcd (x : Long,y : Long) : Long{
-    return if(y == 0L) x else gcd(y,x % y)
+        for (j in 0 until v.size) {//vの配列内=list内はmaxでn^5*5
+            val x = v[j]
+            val m = x + l
+            val mm = x + r
+            var p = v.binarySearch(m)//a[1,3,5]で3なら、１返したい。
+            if (p < 0) p = -(p) - 1
+            var pp = v.binarySearch(mm + 1)//区間の個数出す際+1必須。6はない、3返したい。
+            if (pp < 0) pp = -(pp) - 1
+            count += (pp - p).toLong()//3－1で0から2～4離れた場所に2個ある
+        }
+    }
+    println(count)
 }
