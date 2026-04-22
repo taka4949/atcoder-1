6900287aa09b5968ded18646a4fda1dbbdb363f4
taka4949 <gogigaga1101@outlook.jp>
2026-04-22 17:14:42 +0900

    優先度付きキュー

--- src/Main.kt
+++ src/Main.kt
@@ -9,40 +9,30 @@ import java.util.Arrays
 import java.util.ArrayDeque
 import kotlin.math.max
 import kotlin.system.exitProcess
+import java.util.PriorityQueue
 
-fun main() {
-val (h,w) = readLine()!!.split(" ").map { it.toInt() }
+    fun main() {
 
-    val s = Array(h){readLine()!!}
+        val q = readLine()!!.toInt()
 
-    val a = intArrayOf(-1,0,0,+1)
-    val b = intArrayOf(0,-1,+1,0)
+        // 最小値を効率よく管理する優先度付きキュー
+        // デフォルトで昇順（小さい順）になる
+        val p = PriorityQueue<Int>()
 
+        repeat(q) {
+            val i = readLine()!!.split(" ")
+            if (i[0] == "1") {
 
-    for(i in 0 until h){
-        for(j in 0 until w){
-            var c = 0
-            if(s[i][j] == '#'){
-                for(l in 0 until a.size){
-                    val i1 = i + a[l]
-                    val j1 = j + b[l]
-                    if(i1 < 0 || j1 < 0 || i1 >= h || j1 >= w){
-                        continue//端の場合も、ほかの方向を見る必要がある
-                    }else{
-                        if(s[i1][j1] == '#'){
-                            c++
-                        }
-                    }
-                    }
-                if(!(c == 2 || c == 4)){ //if(#)のときだけチェックするように、if文内で記述
-                    println("No")
-                    return
-                }
+                val x = i[1].toInt()
+                p.add(x)
+            } else {
+                // タイプ 2: 最小値を取り出して出力
+                // poll() は最小値を取得して、そのまま削除
+                println(p.poll())
             }
         }
     }
-    println("Yes")
-}
+
 
 
 
