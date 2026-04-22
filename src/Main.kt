223a02e3756fef9bafbfbc76d066a72936994b3c
taka4949 <gogigaga1101@outlook.jp>
2026-04-22 13:50:48 +0900

    422bグリッド問題基礎

--- src/Main.kt
+++ src/Main.kt
@@ -10,37 +10,40 @@ import java.util.ArrayDeque
 import kotlin.math.max
 import kotlin.system.exitProcess
 
+fun main() {
+val (h,w) = readLine()!!.split(" ").map { it.toInt() }
 
-    fun main() {
-        val (n,m) = readLine()!!.split(" ").map { it.toInt() }
+    val s = Array(h){readLine()!!}
 
-        val g = Array(n + 1){mutableListOf<Int>()}
-        repeat(m){
-            val(a,b) = readLine()!!.split(" ").map { it.toInt() }
-            g[a].add(b)
-        }
-        val v = BooleanArray(n + 1)
-        val stack = ArrayDeque<Int>()
-        var count = 0
+    val a = intArrayOf(-1,0,0,+1)
+    val b = intArrayOf(0,-1,+1,0)
 
-        stack.push(1)
-        v[1] = true
-        count++
 
-        while(stack.isNotEmpty()){
-            val u = stack.pop()
-
-            for(next in g[u]){
-                if(!v[next]){
-                    v[next] = true
-                    count++
-                    stack.push(next)
+    for(i in 0 until h){
+        for(j in 0 until w){
+            var c = 0
+            if(s[i][j] == '#'){
+                for(l in 0 until a.size){
+                    val i1 = i + a[l]
+                    val j1 = j + b[l]
+                    if(i1 < 0 || j1 < 0 || i1 >= h || j1 >= w){
+                        continue//端の場合も、ほかの方向を見る必要がある
+                    }else{
+                        if(s[i1][j1] == '#'){
+                            c++
+                        }
+                    }
+                    }
+                if(!(c == 2 || c == 4)){ //if(#)のときだけチェックするように、if文内で記述
+                    println("No")
+                    return
                 }
             }
         }
-        println(count)
+    }
+    println("Yes")
+}
 
-        }
 
 
 
