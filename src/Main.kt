1f0e4223ffc529d18f9a70a2e48d9ea25d111b82
taka4949 <gogigaga1101@outlook.jp>
2026-04-24 11:34:29 +0900

    255c加算か減算

--- src/Main.kt
+++ src/Main.kt
@@ -19,35 +19,28 @@
 
   fun main() {
     val o = PrintWriter(System.`out`)
-    val br = System.`in`.bufferedReader()
 
-    val (n, q) = br.readLine()!!.split(" ").map { it.toInt() }
+val v = readLine()!!.split(" ")
 
-    val a = IntArray(n + 1)
-    val b = IntArray(n + 1)
+    val x = v[0].toLong()
+    val a = v[1].toLong()
+    val d = v[2].toLong()
+    val n = v[3].toLong()
 
-    for (i in 1..n) {
-      a[i] = i
-      b[i] = i
-    }
+    val m = (n-1L) * d + a
+    val s = min(a,m)
+      val t = max(a,m)
 
-    repeat(q) {
-      val x = br.readLine()!!.trim().toInt()
-      val i = b[x]
-      val j = if (i == n) i - 1 else i + 1
-      val y = a[j]
-
-      a[i] = y
-      a[j] = x
-      b[x] = j
-      b[y] = i
-    }
-
-    val r = StringBuilder()
-    for (h in 1..n) {
-      r.append(a[h]).append(if (h == n) "" else " ")
-    }
-    o.println(r.toString())
+      if(d == 0L){
+          o.println(abs(x-a))
+      }else if(x <= s){
+          o.println(s - x)
+      } else if(x >= t){
+          o.println(x - t)
+      }else{
+          val r = abs(x - a) % abs(d)
+          o.println(min(r,abs(d) - r))
+      }
 
     o.flush()
     o.close()
