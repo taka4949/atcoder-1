a5c30a4a52cb15f646be7bb3cb570f666ae71e65
taka4949 <gogigaga1101@outlook.jp>
2026-05-16 00:28:24 +0900

    430尺とり＋累積和

--- src/Main.kt
+++ src/Main.kt
@@ -16,33 +16,43 @@
   import java.math.BigInteger
   import java.io.PrintWriter
 
-  val m = mutableMapOf<Long, Long>()
 
-  fun f(n: Long): Long {
+fun main () {
+  val f = readLine()!!.split(" ").map { it.toInt() }
+  val n = f[0]
+  val a = f[1]
+  val b = f[2]
+  val s = readLine()!!
 
-    if (n < 2) return 0L
+  var j = 0
+  var k = 0
+  var x = 0
+  var y = 0
+  var z = 0L
 
-    // すでに計算した数字なら、その結果を返す。重要！！！メモ
-    val k = m[n]
-    if (k != null) return k
+  for (i in 0 until n) {
+    while (j < n && x < a) {
+      if (s[j] == 'a') x++//aを必要最低限集める。最小の長さまで
+      j++
+    }
 
-    // ⌊x/2⌋ は n / 2
-    // ⌈x/2⌉ は (n + 1) / 2。ここは切り上げを計算するため
-    val r = n + f(n / 2) + f((n + 1) / 2)
-
-    // 結果を記録して返す
-    m[n] = r
-    return r
+    if (k < i) {//仮に、kがiに追い越されるとき用（保険）
+        k = i
+        y = 0
+    }
+    while(k < n && y < b){
+      if(s[k] == 'b') y++
+      k++
+    }
+    if(x >= a){
+      val r = if(y >= b) k-1 else n //k-1はb個未満だから、1個手前から数える
+      if(r >= j){//考えればわかる、k-1>=jじゃないと条件×
+        z +=(r - j + 1).toLong()//＋1は気にしなくていい
+      }
+    }
+    if(s[i] == 'a') x-- else y--//forループで左に１つずらす。移動する。
   }
-
-  fun main() {
-    val s = readLine()!!
-    val n = s.toLong()
-    println(f(n))
-  }
-
-
-
+}
 
 
 
