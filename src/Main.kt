f539c43c593904c046ffd78a1a9d0773423d0b90
taka4949 <gogigaga1101@outlook.jp>
2026-05-21 17:55:09 +0900

    めぐる式二分テンプレ388c

--- src/Main.kt
+++ src/Main.kt
@@ -1,58 +1,43 @@
-  import kotlin.math.abs
-  import kotlin.math.*
-  import kotlin.math.min
-  import java.util.*
-  import java.util.Scanner
-  import java.util.ArrayList
-  import kotlin.math.max
-  import java.util.Arrays
-  import java.util.ArrayDeque
-  import kotlin.math.max
-  import kotlin.system.exitProcess
-  import java.util.PriorityQueue
-  import java.util.StringTokenizer
-  import java.io.BufferedReader
-  import java.io.InputStreamReader
-  import java.math.BigInteger
-  import java.io.PrintWriter
+import kotlin.math.abs
+import kotlin.math.*
+import kotlin.math.min
+import java.util.*
+import java.util.Scanner
+import java.util.ArrayList
+import kotlin.math.max
+import java.util.Arrays
+import java.util.ArrayDeque
+import kotlin.math.max
+import kotlin.system.exitProcess
+import java.util.PriorityQueue
+import java.util.StringTokenizer
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.math.BigInteger
+import java.util.HashMap
+import java.io.PrintWriter
 
 
-fun main () {
-  val f = readLine()!!.split(" ").map { it.toInt() }
-  val n = f[0]
-  val a = f[1]
-  val b = f[2]
-  val s = readLine()!!
-
-  var j = 0
-  var k = 0
-  var x = 0
-  var y = 0
-  var z = 0L
-
+fun main() {
+  val n = readLine()!!.toInt()
+  val a = readLine()!!.split(" ").map { it.toInt() }.toIntArray()
+  var v = 0L
   for (i in 0 until n) {
-    while (j < n && x < a) {
-      if (s[j] == 'a') x++//aを必要最低限集める。最小の長さまで
-      j++
-    }
-
-    if (k < i) {//仮に、kがiに追い越されるとき用（保険）
-        k = i
-        y = 0
-    }
-    while(k < n && y < b){
-      if(s[k] == 'b') y++
-      k++
-    }
-    if(x >= a){
-      val r = if(y >= b) k-1 else n //k-1はb個未満だから、1個手前から数える
-      if(r >= j){//考えればわかる、k-1>=jじゃないと条件×
-        z +=(r - j + 1).toLong()//＋1は気にしなくていい
+    var l = -1//二分探索スタート位置は参照されない！（だから配列外）
+    var r = n//同様
+    while (r - l > 1) {//めぐる式テンプレ
+      val m = (l + r) / 2//Longなら、ｍ＝l+(r-l)/2
+      if (a[m] >= a[i] * 2) {//最大の最小値ならこの形（m＞＝）になる。重要
+        r = m
+      } else {
+        l = m
       }
     }
-    if(s[i] == 'a') x-- else y--//forループで左に１つずらす。移動する。
+    v += (n - r)
   }
+  println(v)
 }
+
 
 
 
