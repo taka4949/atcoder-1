941d8135799ff69531a112a39d006d1b13dfd543
taka4949 <gogigaga1101@outlook.jp>
2026-05-29 18:22:43 +0900

    380cしゃくとりポインタ間を保存。文字列の応用

--- src/Main.kt
+++ src/Main.kt
@@ -16,38 +16,47 @@ import java.io.InputStreamReader
 import java.math.BigInteger
 import java.util.HashMap
 import java.io.PrintWriter
+import kotlin.math.sqrt
 import kotlin.Comparator
 
 fun main() {
 
+    val v = readLine()!!.split(" ")
+    val n = v[0].toInt()
+    val k = v[1].toInt()
+    val s = readLine()!!
 
-    val v = readLine()!!.split(" ").map { it.toInt() }
-    val c = charArrayOf('A', 'B', 'C', 'D', 'E')
-    val l = mutableListOf<Pair<String, Int>>()
+    var p = 0
+    var c = 0
+    var a = 0
+    var b = 0
+    var d = 0
 
-    for(i in 1..31){
-        var s = ""
-        var p = 0
-        for(j in 0 ..4){
-            if(((i shr j) and 1) !=  0){
-                s += c[j]
-                p += v[j]
+    while(p < n){
+        if(s[p] == '1'){
+            c ++
+            val j = p
+            var e = p
+            while(e < n && s[e] == '1'){
+                e++
             }
+            if(c == k - 1){
+                a = e - 1//k-1番目の1の塊の最後の番号
+            }
+            if(c == k){
+                b = j//k番目の塊の最初の番号
+                d = e - 1
+            }
+            p = e
+        }else{
+            p++
         }
-        l.add(Pair(s,p))
     }
-
-    l.sortWith(Comparator { x, y ->//xとy比較して、プラスなら入れ替える。以外なら何もしない。
-        if (x.second != y.second) {
-            y.second.compareTo(x.second)
-        } else {
-            x.first.compareTo(y.first)
-        }
-    })
-
-for(i in l){
-    println(i.first)
-}
+    val x = s.substring(0,a + 1)
+    val y = s.substring(b,d + 1)
+    val z = s.substring(a + 1,b)
+    val w = s.substring(d + 1)
+    println(x + y + z + w)
 }
 
 
